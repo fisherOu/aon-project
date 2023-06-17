38f73bcf27834723613dbfb7afac96cdadfb3af5
fisherOu <zachuanming@126.com>
2023-06-17 19:08:30 +0800

    fix map_module: warship string memory

--- packages/contracts/src/components/WarshipComponent.sol
+++ packages/contracts/src/components/WarshipComponent.sol
@@ -1,36 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
-import {BareComponent} from "solecs/BareComponent.sol";
-import {LibTypes} from "solecs/LibTypes.sol";
+import { BareComponent } from "solecs/BareComponent.sol";
+import { LibTypes } from "solecs/LibTypes.sol";
 
 uint256 constant ID = uint256(keccak256("component.Warship"));
 
 struct Warship {
-    string model;
+  string model;
 }
 
 contract WarshipComponent is BareComponent {
-    constructor(address world) BareComponent(world, ID) {}
+  constructor(address world) BareComponent(world, ID) {}
 
-    function getSchema()
-        public
-        pure
-        override
-        returns (string[] memory keys, LibTypes.SchemaValue[] memory values)
-    {
-        keys = new string[](1);
-        values = new LibTypes.SchemaValue[](1);
+  function getSchema() public pure override returns (string[] memory keys, LibTypes.SchemaValue[] memory values) {
+    keys = new string[](1);
+    values = new LibTypes.SchemaValue[](1);
 
-        keys[0] = "model";
-        values[0] = LibTypes.SchemaValue.STRING;
-    }
+    keys[0] = "model";
+    values[0] = LibTypes.SchemaValue.STRING;
+  }
 
-    function set(uint256 entity, Warship memory warship) public {
-        set(entity, abi.encode(warship.model));
-    }
+  function set(uint256 entity, Warship memory warship) public {
+    set(entity, abi.encode(warship.model));
+  }
 
-    function getValue(uint256 entity) public view returns (Warship memory) {
-        (string model, ) = abi.decode(getRawValue(entity), (string, ));
-        return Warship(model);
-    }
+  function getValue(uint256 entity) public view returns (Warship memory) {
+    (string memory model, ) = abi.decode(getRawValue(entity), (string, ));
+    return Warship(model);
+  }
 }
